c71f399c60e812304177285f1e61fb3592b381c9
Branis333 <b.sumba@alustudent.com>
2024-06-18 12:00:09 +0200

    waste_management

---
+++ app.py
@@ -0,0 +1,4 @@
+from waste_manage import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ waste_manage/__init__.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+migrate = Migrate(app, db)
+
+from waste_manage import routes, models
+from waste_manage.models import User, Post
+
+# Ensure the database tables are created
+with app.app_context():
+    db.create_all()
+
+# For debugging purposes: Query all users
+with app.app_context():
+    users = User.query.all()
+    print(users)
