a51a07c2ccf3398a874876f8cc100361b4136ab3
Branis333 <b.sumba@alustudent.com>
2024-06-19 12:08:07 +0200

    updated

--- app.py
+++ app.py
@@ -1,17 +1,4 @@
-import os
-from flask import Flask
-from waste_manage.routes import bp
-
-app = Flask(__name__)
-
-# Configuration and other setup if needed
-
-# Register the blueprint with the app
-app.register_blueprint(bp)
-
-@app.route('/')
-def index():
-    return 'Welcome to Our Waste Management Platform'
+from waste_manage import app
 
 if __name__ == '__main__':
     app.run(debug=True)
